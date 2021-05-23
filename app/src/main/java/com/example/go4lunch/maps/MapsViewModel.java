4a251a49721312f208c7d31f826f7dfd969284d9
Thibault Grivel <thibaultgrivel1@gmail.com>
2021-05-23 16:55:12 +0200

    Go4Lunch

--- app/src/main/java/com/example/go4lunch/maps/MapsViewModel.java
+++ app/src/main/java/com/example/go4lunch/maps/MapsViewModel.java
@@ -1,14 +1,7 @@
 package com.example.go4lunch.maps;
-
-import android.app.Activity;
-import android.content.Context;
-
-import androidx.lifecycle.MutableLiveData;
+;
 import androidx.lifecycle.ViewModel;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.model.LatLng;
 
 import java.util.concurrent.Executor;
 
@@ -21,9 +14,4 @@ public class MapsViewModel extends ViewModel {
         this.mMapsRepository = mapsRepository;
         this.mExecutor = executor;
     }
-
-    public void getPosition(Context context) {
-        mExecutor.execute(() -> MapsRepository.getUserLocation(context));
-    }
-
 }
